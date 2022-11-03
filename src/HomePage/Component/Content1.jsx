bf8fb19e90a96cc56055eba4b7081d8893fde89a
rong5026 <rong5026@naver.com>
2022-11-03 17:16:23 +0900

    [UPDATE] 배너 사진 변경

--- src/HomePage/Component/Content1.jsx
+++ src/HomePage/Component/Content1.jsx
@@ -2,154 +2,24 @@ import "../HomePage.css";
 import { useState, useEffect } from "react";
 import styled, { css } from "styled-components";
 
-// const styles = {
-//   wrapper: {
-//     height: "100%",
-//     width: "100%",
-//   },
-//   messageText: {
-//     color: "black",
-//     fontSize: 16,
-//   },
-// };
 
-const SIZES = {
-  small: css`
-    --button-font-size: 0.875rem;
-    --button-padding: 8px 12px;
-    --button-radius: 4px;
-  `,
-  large: css`
-    --button-font-size: 1.25rem;
-    --button-padding: 16px 50px;
-    --button-radius: 12px;
-  `,
-};
+const style = {
+  backGround1 : {
 
-const VARIANTS = {
-  button1: css`
-    --button-color: #ffffff;
-    --button-bg-color: #28a745;
-    --button-hover-bg-color: #218838;
-  `,
-  button2: css`
-    --button-color: #ffffff;
-    --button-bg-color: #dc3545;
-    --button-hover-bg-color: #c82333;
-  `,
-  button3: css`
-    --button-color: #ffffff;
-    --button-bg-color: #ffc107;
-    --button-hover-bg-color: #e0a800;
-  `,
-  button4: css`
-    --button-color: #ffffff;
-    --button-bg-color: #ffc107;
-    --button-hover-bg-color: #e0a800;
-  `,
-  button5: css`
-    --button-color: #ffffff;
-    --button-bg-color: #ffc107;
-    --button-hover-bg-color: #e0a800;
-  `,
-};
-
-const StyledButton = styled.button`
-  ${(p) => p.sizeStyle}
-  ${(p) => p.variantStyle}
-
-  margin: 0;
-  border: none;
-  cursor: pointer;
-  font-family: "Noto Sans KR", sans-serif;
-  font-size: var(--button-font-size, 1rem);
-  padding: var(--button-padding, 12px 16px);
-  border-radius: var(--button-radius, 8px);
-  color: var(--button-color, #ffffff);
-  background: var(--button-bg-color, #0d6efd);
-
-  &:active,
-  &:hover,
-  &:focus {
-    background: var(--button-hover-bg-color, #025ce2);
-  }
-`;
-
-const btnStyle = {
-  button: {
-    margin: 0,
-    border: "none",
-    cursor: "pointer",
-    fontFamily: '"Noto Sans KR", sansSerif',
-    fontSize: "1rem",
-    padding: "12px 16px",
-    borderRadius: "8px",
-    color: "#ffffff",
-    background: "#0d6efd",
-    // &:active,
-    // &:hover,
-    // &:focus {
-    //   background: "#025ce2",
-    // }
   },
+  backGround2 : {
 
-  button1: {
-    margin: 0,
-    border: "none",
-    cursor: "pointer",
-    fontFamily: '"Noto Sans KR", sansSerif',
-    fontSize: "1.25rem",
-    padding: "16px 50px",
-    borderRadius: "12px",
-    color: "#ffffff",
-    background: "#28a745",
   },
-  button1: {
-    margin: 0,
-    border: "none",
-    cursor: "pointer",
-    fontFamily: '"Noto Sans KR", sansSerif',
-    fontSize: "1.25rem",
-    padding: "16px 50px",
-    borderRadius: "12px",
-    color: "#ffffff",
-    background: "#28a745",
-  },
-  button1: {
-    margin: 0,
-    border: "none",
-    cursor: "pointer",
-    fontFamily: '"Noto Sans KR", sansSerif',
-    fontSize: "1.25rem",
-    padding: "16px 50px",
-    borderRadius: "12px",
-    color: "#ffffff",
-    background: "#28a745",
-  },
-  button1: {
-    margin: 0,
-    border: "none",
-    cursor: "pointer",
-    fontFamily: '"Noto Sans KR", sansSerif',
-    fontSize: "1.25rem",
-    padding: "16px 50px",
-    borderRadius: "12px",
-    color: "#ffffff",
-    background: "#28a745",
-  },
-  button1: {
-    margin: 0,
-    border: "none",
-    cursor: "pointer",
-    fontFamily: '"Noto Sans KR", sansSerif',
-    fontSize: "1.25rem",
-    padding: "16px 50px",
-    borderRadius: "12px",
-    color: "#ffffff",
-    background: "#28a745",
-  },
-};
+  backGround3 : {
 
+  },
+  backGround4 : {
+
+  },
+  backGround5 : {
+
+  },
+}
 function Content1() {
   let [btnData, setbtnData] = useState([
     { id: 1, name: "버튼1", active: true },
@@ -158,6 +28,8 @@ function Content1() {
     { id: 4, name: "버튼4", active: false },
     { id: 5, name: "버튼5", active: false },
   ]);
+
+  let [image, setImage] = useState(1);
 
   const onClick = (item) => {
     let result = btnData.map((k) => {
@@ -174,10 +46,11 @@ function Content1() {
       }
     });
     setbtnData(result);
+    setImage((prev) => item.id);
   };
 
   return (
-    <article className="Content1">
+    <article className={"Content1 backGround"+image}>
       <section className="Content1-session">
         <p className="Maintitle">조준희가 추천하는 독서토론~</p>
         <p className="Subtitle">
