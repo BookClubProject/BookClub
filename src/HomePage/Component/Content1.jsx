c67617a08c8b4bc2fb1eb38374e89fa004a8869a
rong5026 <rong5026@naver.com>
2022-11-03 16:39:38 +0900

    [UPDATE] 버튼 각각 색상 설정

--- src/HomePage/Component/Content1.jsx
+++ src/HomePage/Component/Content1.jsx
@@ -1,89 +1,156 @@
 import "../HomePage.css";
 import { useState, useEffect } from "react";
-// import styled, { css } from "styled-components";
+import styled, { css } from "styled-components";
 
-// const SIZES = {
-//   small: css`
-//     --button-font-size: 0.875rem;
-//     --button-padding: 8px 12px;
-//     --button-radius: 4px;
-//   `,
-//   large: css`
-//     --button-font-size: 1.25rem;
-//     --button-padding: 16px 50px;
-//     --button-radius: 12px;
-//   `,
+// const styles = {
+//   wrapper: {
+//     height: "100%",
+//     width: "100%",
+//   },
+//   messageText: {
+//     color: "black",
+//     fontSize: 16,
+//   },
 // };
 
-// const VARIANTS = {
-//   button1: css`
-//     --button-color: #ffffff;
-//     --button-bg-color: #28a745;
-//     --button-hover-bg-color: #218838;
-//   `,
-//   button2: css`
-//     --button-color: #ffffff;
-//     --button-bg-color: #dc3545;
-//     --button-hover-bg-color: #c82333;
-//   `,
-//   button3: css`
-//     --button-color: #ffffff;
-//     --button-bg-color: #ffc107;
-//     --button-hover-bg-color: #e0a800;
-//   `,
-//   button4: css`
-//     --button-color: #ffffff;
-//     --button-bg-color: #ffc107;
-//     --button-hover-bg-color: #e0a800;
-//   `,
-//   button5: css`
-//     --button-color: #ffffff;
-//     --button-bg-color: #ffc107;
-//     --button-hover-bg-color: #e0a800;
-//   `,
-// };
+const SIZES = {
+  small: css`
+    --button-font-size: 0.875rem;
+    --button-padding: 8px 12px;
+    --button-radius: 4px;
+  `,
+  large: css`
+    --button-font-size: 1.25rem;
+    --button-padding: 16px 50px;
+    --button-radius: 12px;
+  `,
+};
 
-// const StyledButton = styled.button`
-//   ${(p) => p.sizeStyle}
-//   ${(p) => p.variantStyle}
+const VARIANTS = {
+  button1: css`
+    --button-color: #ffffff;
+    --button-bg-color: #28a745;
+    --button-hover-bg-color: #218838;
+  `,
+  button2: css`
+    --button-color: #ffffff;
+    --button-bg-color: #dc3545;
+    --button-hover-bg-color: #c82333;
+  `,
+  button3: css`
+    --button-color: #ffffff;
+    --button-bg-color: #ffc107;
+    --button-hover-bg-color: #e0a800;
+  `,
+  button4: css`
+    --button-color: #ffffff;
+    --button-bg-color: #ffc107;
+    --button-hover-bg-color: #e0a800;
+  `,
+  button5: css`
+    --button-color: #ffffff;
+    --button-bg-color: #ffc107;
+    --button-hover-bg-color: #e0a800;
+  `,
+};
 
-//   margin: 0;
-//   border: none;
-//   cursor: pointer;
-//   font-family: "Noto Sans KR", sans-serif;
-//   font-size: var(--button-font-size, 1rem);
-//   padding: var(--button-padding, 12px 16px);
-//   border-radius: var(--button-radius, 8px);
-//   color: var(--button-color, #ffffff);
-//   background: var(--button-bg-color, #0d6efd);
+const StyledButton = styled.button`
+  ${(p) => p.sizeStyle}
+  ${(p) => p.variantStyle}
 
-//   &:active,
-//   &:hover,
-//   &:focus {
-//     background: var(--button-hover-bg-color, #025ce2);
-//   }
-// `;
+  margin: 0;
+  border: none;
+  cursor: pointer;
+  font-family: "Noto Sans KR", sans-serif;
+  font-size: var(--button-font-size, 1rem);
+  padding: var(--button-padding, 12px 16px);
+  border-radius: var(--button-radius, 8px);
+  color: var(--button-color, #ffffff);
+  background: var(--button-bg-color, #0d6efd);
 
-// function Button({ size, variant, value,btnActive,onClick, children }) {
-//     const sizeStyle = SIZES[size];
-//     const variantStyle = VARIANTS[variant];
+  &:active,
+  &:hover,
+  &:focus {
+    background: var(--button-hover-bg-color, #025ce2);
+  }
+`;
 
-//     console.log("다시실행됨");
-//     return (
-//       <StyledButton
-//         sizeStyle={sizeStyle}
-//         variantStyle={variantStyle}
-//         value = {value}
-//         onClick = {onClick}
-//         className={"btn" + value + (value === btnActive ? " active" : "")}
-//       >
-//         {children}
-//       </StyledButton>
-//     );
-//   }
+const btnStyle = {
+  button: {
+    margin: 0,
+    border: "none",
+    cursor: "pointer",
+    fontFamily: '"Noto Sans KR", sansSerif',
+    fontSize: "1rem",
+    padding: "12px 16px",
+    borderRadius: "8px",
+    color: "#ffffff",
+    background: "#0d6efd",
+    // &:active,
+    // &:hover,
+    // &:focus {
+    //   background: "#025ce2",
+    // }
+  },
+
+  button1: {
+    margin: 0,
+    border: "none",
+    cursor: "pointer",
+    fontFamily: '"Noto Sans KR", sansSerif',
+    fontSize: "1.25rem",
+    padding: "16px 50px",
+    borderRadius: "12px",
+    color: "#ffffff",
+    background: "#28a745",
+  },
+  button1: {
+    margin: 0,
+    border: "none",
+    cursor: "pointer",
+    fontFamily: '"Noto Sans KR", sansSerif',
+    fontSize: "1.25rem",
+    padding: "16px 50px",
+    borderRadius: "12px",
+    color: "#ffffff",
+    background: "#28a745",
+  },
+  button1: {
+    margin: 0,
+    border: "none",
+    cursor: "pointer",
+    fontFamily: '"Noto Sans KR", sansSerif',
+    fontSize: "1.25rem",
+    padding: "16px 50px",
+    borderRadius: "12px",
+    color: "#ffffff",
+    background: "#28a745",
+  },
+  button1: {
+    margin: 0,
+    border: "none",
+    cursor: "pointer",
+    fontFamily: '"Noto Sans KR", sansSerif',
+    fontSize: "1.25rem",
+    padding: "16px 50px",
+    borderRadius: "12px",
+    color: "#ffffff",
+    background: "#28a745",
+  },
+  button1: {
+    margin: 0,
+    border: "none",
+    cursor: "pointer",
+    fontFamily: '"Noto Sans KR", sansSerif',
+    fontSize: "1.25rem",
+    padding: "16px 50px",
+    borderRadius: "12px",
+    color: "#ffffff",
+    background: "#28a745",
+  },
+};
 
 function Content1() {
-
   let [btnData, setbtnData] = useState([
     { id: 1, name: "버튼1", active: true },
     { id: 2, name: "버튼2", active: false },
@@ -93,24 +160,21 @@ function Content1() {
   ]);
 
   const onClick = (item) => {
-
-    let result = btnData.map(k => {
-      console.log("k의 id값 " + k.id + "event" + item.id);
-      if(k.id ===item.id) {
+    let result = btnData.map((k) => {
+      if (k.id === item.id) {
         return {
           ...k,
-          active : true,
+          active: true,
         };
-      }
-      else {
+      } else {
         return {
           ...k,
-          active : false,
+          active: false,
         };
       }
     });
     setbtnData(result);
-  }
+  };
 
   return (
     <article className="Content1">
@@ -122,7 +186,22 @@ function Content1() {
         <div className="Content-btn">
           <ul>
             {btnData.map((item, idx) => {
-              return <button key={item.id} onClick={ () => onClick(item)} style = { item.active === true ? {backgroundColor : "yellow"} :{backgroundColor : "blue"}}>{item.name} </button>;
+              return (
+                <button
+                  key={item.id}
+                  onClick={() => onClick(item)}
+                  className ={(item.active === true
+                         ? "category_btn " + "active on" +item.id
+                         :"category_btn " + item.id)}
+                  // style={
+                  //   (item.active === true
+                  //     ? { backgroundColor: "yellow" }
+                  //     : btnStyle.button)
+                  // }
+                >
+                  {item.name}
+                </button>
+              );
             })}
           </ul>
         </div>
