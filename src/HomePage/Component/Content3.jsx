9e013997ba0754ae9a1de574320293c587186d00
hong <rong5026@naver.com>
2022-11-09 14:24:08 +0900

    [ADD] 가운데 후기부분 틀완성

--- src/HomePage/Component/Content3.jsx
+++ src/HomePage/Component/Content3.jsx
@@ -1,22 +1,54 @@
-
 import "../HomePage.css";
 
 function Content3() {
-
-
-    return (
-        <div class = "Content3_wrap">
-            <div className="content3_left">
-                여기는 세로로 3칸나눠서 쓸만한거 쓰기 
-            </div>
-
-            <div className="conten3_right">
-                여기는 한페이지 전체를 후기볼수있게 화살표로 넘기면서
-            </div>
-            
-             
+  return (
+    <div className="content3_wrap">
+      <div className="content3_left">
+        <p className="content3_left_title">베스트 독서 노트</p>
+        <div className="content3_list">
+          <ul>
+            <li className="report_li">
+              <a href="#" className="best_report1">
+                <div className="info">
+                  <h3>(체호프 희곡 낭독후기) 알찬 시간이었다</h3>
+                  <p></p>
+                </div>
+                <div className="image">
+                  <img></img>
+                </div>
+              </a>
+            </li>
+            <li className="report_li">
+              <a href="#" className="best_report2">
+                <div className="info">
+                  <p>제목</p>
+                  <p>내용물</p>
+                </div>
+                <div className="image">
+                  <img></img>
+                </div>
+              </a>
+            </li>
+            <li className="report_li">
+              <a href="#" className="best_report3">
+                <div className="info">
+                  <p>제목</p>
+                  <p>내용물</p>
+                </div>
+                <div className="image">
+                  <img></img>
+                </div>
+              </a>
+            </li>
+          </ul>
         </div>
-    );
+      </div>
+      <div className="conten3_right">
+        <p> 토론 후기 </p>
+        <div> 내용</div>
+      </div>
+    </div>
+  );
 }
 
 export default Content3;
