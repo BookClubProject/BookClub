6f5b0ff4c49e994bb06dbc52224db4054f0f8e1b
조준희 <iopp3423@gmail.com>
2022-11-09 16:48:41 +0900

    [ADD] 테스트 페이지에 api 불러오는 코드 추가 및 env파일에 시크릿 키 감추는 파일 및 코드 작성

--- src/test.jsx
+++ src/test.jsx
@@ -6,34 +6,61 @@ function Test(){
     const [api, setApi] = useState('');
 
     const option ={
-        url : 'https://openapi.naver.com/v1/search/book.json',
-         method:'POST',
-         header:{
-           'Accept':'application/json',
-           'Content-Type':'application/json charset=UTP-8'
-        },
-        data:{
-            name:'sewon',
-              age:20
+            url : 'https://openapi.naver.com/v1/search/book.json',
+            params:{
+                query: '삼국지',
+                display: 10,
+                start : 1,
+              },
+            headers: {
+                'X-Naver-Client-Id':process.env.REACT_APP_CLIENT_ID,
+                'X-Naver-Client-Secret': process.env.REACT_APP_CLIENT_SECRET,
+            },
         }
-    }
+
         {/**API 가져오기 */}
     const getBookApi = async() =>{
         try {
-        const getClubList = await (await axios.get("https://openapi.naver.com/v1/search/book.json"));
-        setApi(getClubList.data); {/** api 가져오기 */} 
+        const api = await (await axios.get(option));
+        setApi(api); {/** api 가져오기 */} 
+        console.log(api);
         } catch {
         console.log("error");
         // 오류 발생시 실행
         }
     }
+    const ID_KEY = 'mT8QQrmD4w7I5tcpqexQ';
+    const SECRET_KEY = 'oh6xEebpVb';
+    const search = '검사외전';
+
+    const getSearchMovie = async () => {
+        try {
+            const items = await axios.get('v1/search/book.json',{
+                params:{
+                  query: '노르웨이의 숲',
+                  display: 1,
+                  start : 1,
+                  sort : 'sim'
+                },
+                headers: {
+                  'X-Naver-Client-Id': ID_KEY,
+                  'X-Naver-Client-Secret': SECRET_KEY
+                },
+              }).then((response) => {
+                console.log('response', response.data);  // 영화 리스트
+              }); 
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
     useEffect(() => {
-        getBookApi();
+        getSearchMovie();
       }, []);
     return(
         <div>
             asdkasd;klasl;
-            
+            {api.title}
         </div>
     );
 }
