6c418551e4bb184bc5c97084bab8bf1a69f70f91
rong5026 <rong5026@naver.com>
2022-11-10 00:23:19 +0900

    [UPDATE] 베스트독서, 토론 후기 파트

--- src/HomePage/Component/Content2.jsx
+++ src/HomePage/Component/Content2.jsx
@@ -1,12 +1,14 @@
 import "../HomePage.css";
 function Content2() {
-
-    return (
-        <div>
-              <div className= "Content2"> Content2</div>
-        <button> 나에게 맞는 독서찾으러가기 위에는 준희형 리스트 보여주기</button>
+  return (
+    <div className="Content2">
+        <div className="content2_wrap">
+        <div > Content2</div>
+      <button> 나에게 맞는 독서찾으러가기 위에는 준희형 리스트 보여주기</button>
         </div>
-    );
+    
+    </div>
+  );
 }
 
 export default Content2;
