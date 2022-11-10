3c85a266fda2e5c56aa16c3d8caa603b5a4302e8
rong5026 <rong5026@naver.com>
2022-11-10 22:44:59 +0900

    [UPDATE] 예약리스트 틀

--- src/HomePage/Component/Content2.jsx
+++ src/HomePage/Component/Content2.jsx
@@ -2,11 +2,14 @@ import "../HomePage.css";
 function Content2() {
   return (
     <div className="Content2">
-        <div className="content2_wrap">
-        <div > Content2</div>
-      <button> 나에게 맞는 독서찾으러가기 위에는 준희형 리스트 보여주기</button>
+      <div className="content2_wrap">
+        <div className="content2_book_list_wrap">
+          <div className="content2_book_list"> Content2</div>
         </div>
-    
+      </div>
+      <div className="content2_btn_wrap">
+        <button className="content2_add_look_btn">더보기</button>
+      </div>
     </div>
   );
 }
