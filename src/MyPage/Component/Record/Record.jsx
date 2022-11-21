c56401004e39c738929b7e40bb96b145b14fa2d3
rong5026 <rong5026@naver.com>
2022-11-21 13:26:13 +0900

    [ADD] 토론참여 페이지

--- src/MyPage/Component/Record/Record.jsx
+++ src/MyPage/Component/Record/Record.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import "../../CompoCSS/Record.css"
+import React from "react";
+import "../../CompoCSS/Record.css";
+import DropdownList from "react-widgets/DropdownList";
 function Record() {
-  return (
-    <div className='record'>
-        <div className='record_wrap'>
+  const selectBarData = [
+    { id: 0, name: "토론내역" },
+    { id: 1, name: "토론취소" },
+  ];
 
+  return (
+    <div className="record">
+      <div className="record_wrap">
+        <div className="record_title">
+          <div className="record_select_bar">
+            <DropdownList
+              dataKey="id"
+              textField="name"
+              defaultValue={["토론내역"]}
+              data={selectBarData}
+            />
+          </div>
         </div>
-      
+
+        <div className="record_main"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Record
+export default Record;
